be678a131d85b58702931739e30db17063f1dd6c
Joe Izenman <jizenman@sitecrafting.com>
2017-08-12 00:53:55 -0700

    Preliminary analysis and graphics

---
+++ analysis.R
@@ -0,0 +1,74 @@
+library(reshape2)
+library(data.table)
+library(stargazer)
+
+####################
+# DATA PREPARATION #
+####################
+
+# Generate vectors to identify different question types
+presurvey_results <- fread("data/presurvey_results.csv")
+q_positive <- presurvey_results[preferred_response == 1 & significant_05, as.character(variable)]
+q_negative <- presurvey_results[preferred_response == 2 & significant_05, as.character(variable)]
+q_selected <- c("Q1",  "Q2",  "Q3",  "Q5",  "Q6",  "Q7",  "Q9",  "Q10", 
+                "Q11", "Q13", "Q14", "Q15", "Q16", "Q18", "Q19", "Q20")
+
+# Load the responses
+main_responses_long = fread("data/main_responses_long.csv")
+main_responses_wide = fread("data/main_responses_wide.csv")
+
+# Aggregate per-respondent answers to question types
+main_responses_wide[, yes_all := rowSums(.SD), .SDcols=q_selected]
+main_responses_wide[, yes_pos := rowSums(.SD), .SDcols=q_positive]
+main_responses_wide[, yes_neg := rowSums(.SD), .SDcols=q_negative]
+
+# Flag individual question answers with question type
+questions_long = main_responses_long[
+  variable %in% q_selected, 
+  .(ResponseId, variable, value, "negative"=as.integer(variable %in% q_negative))
+  ]
+
+# ANALYSIS ONE: IMPACT OF TIMING
+# Q1: Is there a difference in email disclosure between pre and post?
+a1.q1 <- t.test(email ~ email_timing, main_responses_wide)
+a1.q1
+
+# Q2: Is there a difference in "Yes" answers between pre and post?
+a1.q2 <- t.test(yes_all ~ email_timing, main_responses_wide)
+a1.q2
+
+# Q3: Is there a difference in "Yes" answers on invasive questions
+#     between pre and post?
+a1.q3 <- t.test(yes_neg ~ email_timing, main_responses_wide)
+a1.q3
+
+# ANALYSIS TWO: IMPACT OF QUESTIONS ON ANSWERS
+# Q1: Does the invasiveness of a question impact the number of yes 
+#     answers it receives?
+
+a2.q1 <- t.test(value ~ negative, questions_long)
+a2.q1
+
+# ANALYSIS THREE: IMPACT OF QUESTIONS ON DISCLOSURE
+# Q1: In the post group, is there a difference in email disclosure caused 
+#     by the number of "Yes" answers?
+a3.q1 <- lm(email ~ yes_all, main_responses_wide[email_timing==1])
+summary(a3.q1)
+
+# Q2: In the post group, is there a difference in email disclosure caused
+#     by the number of invasive questions answered "Yes"?
+a3.q2 <- lm(email ~ yes_neg, main_responses_wide[email_timing==1])
+summary(a3.q2)
+
+# ANALYSIS FOUR: IMPACT OF DISCLOSURE ON QUESTIONS
+# Q1: In the pre group, is there a difference in number of "Yes" answers
+#     caused by email disclosure?
+a4.q1 <- t.test(yes_all ~ email, main_responses_wide[email_timing==0])
+a4.q1
+
+
+# Q2: In the pre group, is there a difference in number of "Yes" answers
+#     to invasive questions caused by email disclosure?
+a4.q2 <- lm(yes_neg ~ email, main_responses_wide[email_timing==0])
+summary(a4.q2)
+
---
+++ graphics.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+
+questions_long[, .("value"=sum(value, na.rm=TRUE)), by=variable]
+q_all <- c("Q1",  "Q2",  "Q3",  "Q4",  "Q5",
+           "Q6",  "Q7",  "Q8",  "Q9",  "Q10",
+           "Q11", "Q12", "Q13", "Q14", "Q15", 
+           "Q16", "Q17", "Q18", "Q19", "Q20")
+
+color_pos <- "#56B4E9"
+color_neg <- "#E69F00"
+
+# Reponse rate to questions, colored by type
+ggplot(
+  questions_long[, .("value"=sum(value, na.rm=TRUE)), by=variable], 
+  aes(factor(variable, levels=q_all), value, fill=(variable %in% q_positive))
+  ) +
+  geom_col() +
+  scale_fill_manual(name="Question Type", values=c(color_neg, color_pos), labels=c("Socially Unacceptable", "Socially Acceptable")) +
+  labs(x="Question", y="Yes answers") +
+  theme_minimal() +
+  theme(axis.text.x=element_text(angle=45, hjust=1))
+
+# Presurvey results
+ggplot(presurvey_results) +
+  geom_col(aes(factor(variable, levels=q_all), response_1, alpha=(significant_05 & preferred_response == 1), fill="pos")) +
+  geom_col(aes(factor(variable, levels=q_all), response_2*-1, alpha=(significant_05 & preferred_response == 2), fill="neg")) +
+  scale_y_continuous(labels=abs) +
+  scale_alpha_discrete(name="p < .05", range=c(0.5, 1.0)) +
+  scale_fill_manual(name="Question Type", values=c(pos=color_pos, neg=color_neg), labels=c(pos="Socially Acceptable", neg="Socially Unacceptable")) +
+  theme_minimal() +
+  theme(axis.text.x=element_text(angle=45, hjust=1)) +
+  labs(x="Question", y="Presurvey Responses")
